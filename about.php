62a04f04400d278bb36617a2162b716d8826f6a3
saikatgomes <saikatgomes@gmail.com>
2014-11-18 13:48:47 -0600

    new changes

--- about.php
+++ about.php
@@ -2,6 +2,7 @@
 	<div class="row">
 		<div class="col-md-12">
 		    <h1>		
+				<br>
 				<font size="100"><font color="d9230f">CONNECT</font><font color="000000"> + </font><font color="d9230f">ENGAGE</font><font color="000000"> = </font><font color="d9230f">TRANSFORM</font></font>
 			</h1>
 		</div>
