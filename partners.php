926ab0820ab55536edd3b86dca854ec6759f2ff0
Yashwardhan Singh <yashward@macaroni-01.cs.wisc.edu>
2014-12-08 18:59:13 -0600

    Made a small change in partners.php

--- partners.php
+++ partners.php
@@ -12,13 +12,16 @@
 		<div class="col-md-1">
 			&nbsp;
 		</div>
+		<div class="col-md-1">
+			&nbsp;
+		</div>
 		<div class="col-md-3">
 			<img src="img/SOE.jpg" height="175">
 		</div>
 		<div class="col-md-3">
 			<img src="img/WDPI.jpg" height="175">
 		</div>
-		<div class="col-md-3">
+		<div class="col-md-2">
 			<img src="img/WCER.jpg" height="175">
 		</div>
 		<div class="col-md-1">
